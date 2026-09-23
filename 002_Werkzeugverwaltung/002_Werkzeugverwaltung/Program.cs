e4e68ef0bed32bdec0ff7e2bc929ca2663b6f1a3
Florian Nürnberger <florian.nuernberger@htlwy.at>
2026-09-23 13:56:54 +0200

    erste version der Werkzeugverwaltung

---
+++ 002_Werkzeugverwaltung/002_Werkzeugverwaltung/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _002_Werkzeugverwaltung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
